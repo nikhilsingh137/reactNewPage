efab72ae458c3a4f4c002bd0fad0b0b2677cc00f
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2025-02-08 17:26:21 +0530

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import App from "./mainComponent/App";
 
 const root = ReactDOM.createRoot(
--- src/mainComponent/App.tsx
+++ src/mainComponent/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../common-component/Header";
-
 import Footer from "../common-component/Footer";
+import { RouterProvider } from "react-router";
+import router from "../Router";
 
 const App = () => {
   return (
     <>
       <Header />
+      <RouterProvider router={router} />
       <Footer />
     </>
   );
