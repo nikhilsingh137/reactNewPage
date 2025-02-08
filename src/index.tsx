2a9fae0c5f1eafbd50a6a341c6e3bf23d87dcd0c
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2025-02-08 17:17:30 +0530

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./mainComponent/App";
+import Header from "./common-component/Header";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>
 );
