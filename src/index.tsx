6c50ef4a2685952646b4908af69e7ae04217c48d
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2025-02-08 17:21:49 +0530

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Header from "./common-component/Header";
+
+import App from "./mainComponent/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
 );
--- src/mainComponent/App.tsx
+++ src/mainComponent/App.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Header from "../common-component/Header";
-import { RouterProvider } from "react-router-dom";
-import router from "../Router";
+
 import Footer from "../common-component/Footer";
-import NewHeader from "../common-component/NewHeader";
 
 const App = () => {
-  const location = window.location.pathname === "/login";
-  const location1 = window.location.pathname === "/register";
-  const location2 = window.location.pathname === "/";
-
   return (
     <>
-      {!location && !location1 && (location2 ? <Header /> : <NewHeader />)}
-      <RouterProvider router={router} />
-      {!location && !location1 && <Footer />}
+      <Header />
+      <Footer />
     </>
   );
 };
