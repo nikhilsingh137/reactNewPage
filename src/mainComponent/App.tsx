826f81a9efbad8d7411f1d4936a1679a8f90a903
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2025-02-18 18:35:27 +0530

    update file

--- src/mainComponent/App.tsx
+++ src/mainComponent/App.tsx
@@ -3,11 +3,13 @@ import Header from "../common-component/Header";
 import { RouterProvider } from "react-router-dom";
 import router from "../Router";
 import Footer from "../common-component/Footer";
+import HomePage from "../page/HomePage";
 
 const App = () => {
   return (
     <>
       <Header />
+      <HomePage />
       <RouterProvider router={router} />
       <Footer />
     </>
