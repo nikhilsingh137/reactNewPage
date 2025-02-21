37e2763e1242cd371ecac6f089e041f955d1ea3d
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2025-02-21 12:45:32 +0530

    update file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./mainComponent/App";
-import FormPost from "./FormPost";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,9 +9,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <FormPost />
-    {/* <Login />
-    <ChangePassword />
-    <LogOut /> */}
   </React.StrictMode>
 );
